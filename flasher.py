d6f43858ea5f434aad03754340ab5ad2a4416481
Guidenevada <67476134+Guidenevada@users.noreply.github.com>
2023-06-25 22:45:03 +0200

    Update flasher.py

--- flasher.py
+++ flasher.py
@@ -4,27 +4,47 @@ from termcolor import colored
 import time
 
 def detect_images(): #function to detect images in the current directory
-    images = []
-    for file in os.listdir(os.getcwd()):
-        if file.endswith(".img"):
-            images.append(file)
+    
+    if os.listdir(os.getcwd()) == "images.txt":
+        images = ["1"]
+        with open("images.txt", "r") as f:
+            for line in f:
+                images.append(line)
+    else:
+        images = ["0"]    
+        for file in os.listdir(os.getcwd()):
+            if file.endswith(".img"):
+                images.append(file)
     return images
 
 def flash(): #function to flash an android devices with every possible image in fastboot mode
     images = detect_images()
-    print(colored("Detected images: ", "green"))
-    for image in images:
-        
-        print(colored(image,"blue"))
-    for image in images:
-        
-        print(colored("Flashing " + image.split(".")[0] + "...", "green"))
-        os.system("fastboot flash " + image.split(".")[0] + " " + image)
-        print(colored("Done!", "blue"))
-    print(colored("Ereasing storage", "green"))
-    os.system("fastboot -w")
-    time.sleep(5)
-    os.system("fastboot reboot")
+    if images[0] == "1":
+        print(colored("Detected images: ", "green"))
+        for image in images[1:]:
+
+            print(colored(image,"blue"))
+        for image in images:
+
+            print(colored("Flashing " + image.split(".")[0] + "...", "blue"))
+            os.system("fastboot flash " + image.split(".")[0] + " " + image)
+            print(colored("Done!", "green"))
+        print(colored("Ereasing storage", "blue"))
+        os.system("fastboot -w")
+        time.sleep(5)
+        os.system("fastboot reboot")
+    if images[0] == "0":
+        print(colored("Using images from images.txt", "green"))
+        print(colored("images ares", "blue"))
+        # print every image in images.txt exept the first line
+        for image in images[1:]:
+            image = image.split("/")    
+            print(colored(image[0]+ "in" + image[1],"blue"))
+        for image in images[1:]:
+            image = image.split("/")
+            print(colored("Flashing " + image[0] + "...", "blue"))
+            os.system("fastboot flash " + image[1] + " " + image[0])
+            print(colored("Done!", "green"))
 
 def main():
     flash()
